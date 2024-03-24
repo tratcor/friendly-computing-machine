9e099b7ec0d4c8ef156960fcddffa76c5577beeb
tratcor <75759696+tratcor@users.noreply.github.com>
2024-03-24 13:19:49 +0100

    Add files via upload
    App + datafile

---
+++ app_ufo_sightings.py
@@ -0,0 +1,25 @@
+
+
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+#df = pd.read_csv('X:/Current/Dev/Python/Visualisation Sandbox/UFO Sightings/ufo_sightings_scrubbed_small.csv')
+df = pd.read_csv('X:/Current/Dev/Python/Visualisation Sandbox/UFO Sightings/ufo_sightings_scrubbed.csv', low_memory=False)
+                 
+fig = px.pie(df, values='count', names='country', title='UFO per Country', color_discrete_sequence=px.colors.sequential.RdBu)
+fig.update_traces(textposition='inside', textinfo='percent+label')
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
